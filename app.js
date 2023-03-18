de5a00923fd3e22d9708ff3840a2766b807a83b3
Akshay1125 <akshaybhoite1125@gmail.com>
2023-03-19 02:43:51 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-var Items = ["Buy Food", "Cook Food", "Eat Food"];
-var workItems = [];
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }) );
@@ -9,40 +7,38 @@ app.use(bodyParser.urlencoded({ extended: true }) );
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+let listss1 =[];
+let listss2 =[];
+let listss3 =[];
+
+
 
 app.get('/', (req, res) => {
 
-    var today = new Date();
-    
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    var day = today.toLocaleDateString("en-US", options);
-    
 
-    res.render('list', {kindOfDay: day, newvalue : Items });
+    res.render('list1', {listss1 : listss1 , listss2 : listss2 , listss3 : listss3 });
 
 });
 
 app.post('/', (req, res) => {
-    
-    let Item = req.body.item;
-    if(req.body.List === "Work") {
-        workItems.push(Item);
-        res.redirect('/work');
-    } else {
-        Items.push(Item);
+  
+    let Item1 = req.body.text1;
+    let Item2 = req.body.text2;
+    let Item3 = req.body.text3;
 
-        res.redirect('/');}
+    console.log(Item1);
+    console.log(Item2);
+    console.log(Item3);
     
+    if(Item1!==undefined) {listss1.push(Item1);}
+    if(Item2!==undefined){ listss2.push(Item2);}
+    if(Item3!==undefined){ listss3.push(Item3);}
+    res.redirect('/');
+    
+
 });
 
-app.get('/work', function(req, res) {
-    res.render('list', {kindOfDay: "Work List", newvalue : workItems });
-    
-});
+
 
 
 app.listen(process.env.PORT ||3000, function() {
@@ -50,3 +46,8 @@ app.listen(process.env.PORT ||3000, function() {
     console.log('Server is running on port 3000');
 
 });
+
+
+//https://github.com/Akshay1125/ToDoList1
+
+//https://www.linkedin.com/posts/activity-7039291019539931136-e11U?utm_source=share&utm_medium=member_desktop
